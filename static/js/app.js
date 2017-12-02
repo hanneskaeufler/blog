9da7df03709ec0dd5e7466ba7983c0759615997c
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-02 23:25:02 +0100

    finally a working gist embed solution

--- static/js/app.js
+++ static/js/app.js
@@ -2,19 +2,29 @@
 import RailsUjs from "rails-ujs"
 import Turbolinks from "turbolinks"
 
+// gist embeds are wonky because of document.write
 // https://gist.github.com/danshearmur/1993718
 document._write = document.write;
+var addedStyleToHead = false;
 document.write = function(str) {
-  if (str.indexOf('<div id=\"gist-') != 0) {
-    if(str.indexOf('https://gist.github.com/stylesheets/gist/embed.css') == -1) {
-      // if you got this far it's not a github document.write call
-      document._write(str);
+  // gist css
+  if (str.indexOf('<link rel="stylesheet"') == 0) {
+    // just add the stylesheet to the head
+    if (!addedStyleToHead) {
+      document.head.innerHTML += str;
+      addedStyleToHead = true;
     }
-    return;
   }
-  var id = /<div id=\"gist-(\d+)/.exec(str)[1];
-  var script = document.querySelector('script[src="https://gist.github.com/' + id + '.js"]');
-  var div = document.createElement('div');
-  div.innerHTML = str;
-  script.parentNode.insertBefore(div, script.nextElementSibling);
+  // gist html
+  else if (str.indexOf('<div id=\"gist') == 0) {
+    var idMatch = /<a href=\"https:\/\/gist\.github\.com\/hanneskaeufler\/(\w+)\//.exec(str);
+    var id = idMatch[1];
+    var script = document.querySelector('script[src="https://gist.github.com/hanneskaeufler/' + id + '.js"]');
+    var div = document.createElement('div');
+    div.innerHTML = str;
+    script.parentNode.insertBefore(div, script.nextElementSibling);
+  }
+  else {
+    document._write(str);
+  }
 }
