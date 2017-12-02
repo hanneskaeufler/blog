0b0330c60d9dc8b2f957731aee6ec748e1bc307f
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-02 17:44:09 +0100

    design an amazing footer

--- src/pages/blog/index.cr
+++ src/pages/blog/index.cr
@@ -104,6 +104,16 @@ class Blog::IndexPage < MainLayout
         text "89610 Oberdischingen"
       end
       div do
+        h4 "From Germany with ❤️"
+        text "This blog is open source, check it out on "
+        link "Github", to: "https://github.com/hanneskaeufler/blog"
+        text ". It is served by "
+        link "Heroku", to: "https://heroku.com"
+        text ", written in "
+        link "Crystal", to: "https://crystal-lang.org"
+        text " and built with the awesome "
+        link "Lucky Framework", to: "https://luckyframework.org"
+        text "."
       end
     end
   end
