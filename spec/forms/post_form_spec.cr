6be7ca2efd87b23a7fc520c6262bfb909754a273
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-04 20:48:51 +0100

    add test for the form class, dont like it though

--- spec/forms/post_form_spec.cr
+++ spec/forms/post_form_spec.cr
@@ -1,7 +1,14 @@
+require "../spec_helper"
+
 describe PostForm do
   it "allows a title and some content" do
-    # params = { :post => Post.new }
-    # PostForm.create(params) do
-    # end
+    body = <<-BODY
+    #{URI.escape("post:title")}=a&#{URI.escape("post:content")}=b
+    BODY
+    request = HTTP::Request.new("POST", "/irrelevant", nil, body)
+    params = Lucky::Params.new(request)
+    PostForm.create(params) do |form, post|
+      PostQuery.new.count.should eq 1
+    end
   end
 end
