7db7a8694317a2bff42b163cd3010ef9f28741d1
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-04 21:53:38 +0100

    naive approach at blocking the two endpoints

--- src/actions/blog/posts/create.cr
+++ src/actions/blog/posts/create.cr
@@ -1,5 +1,7 @@
 class Blog::Posts::Create < BrowserAction
   post "/posts/create" do
+    return render_text "Access denied" if Lucky::Env.production?
+
     PostForm.create(params) do |form, post|
       if post
         redirect Blog::Index
--- src/actions/blog/posts/new.cr
+++ src/actions/blog/posts/new.cr
@@ -1,5 +1,7 @@
 class Blog::Posts::New < BrowserAction
   get "/posts/new" do
+    return render_text "Access denied" if Lucky::Env.production?
+
     render Blog::Posts::NewPage, post_form: PostForm.new
   end
 end
