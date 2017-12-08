a85c2614a1516058e3fe13a986b89e35e8999749
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-08 21:34:20 +0100

    use experimental optional_param in lucky branch

--- src/actions/blog/index.cr
+++ src/actions/blog/index.cr
@@ -1,13 +1,8 @@
 class Blog::Index < BrowserAction
-  get "/" do
-    page = params.get(:page)
-    if page
-      page = page.to_i
-    else
-      page = 1
-    end
+  optional_param page, default: "1"
 
-    posts = PostQuery.new.latest(page: page).results
+  get "/" do
+    posts = PostQuery.new.latest(page: page.to_i).results
     render Blog::IndexPage, posts: posts
   end
 end
