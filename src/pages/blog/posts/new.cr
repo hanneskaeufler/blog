01c5b8f95ef7537cd09d68503f8148ab9d4f67bc
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-17 18:52:43 +0100

    add footer as well

--- src/pages/blog/posts/new.cr
+++ src/pages/blog/posts/new.cr
@@ -16,6 +16,7 @@ class Blog::Posts::NewPage < MainLayout
     section class: "post-editor" do
       render_post_form(@post_form)
     end
+    render_footer
   end
 
   private def render_post_form(f)
