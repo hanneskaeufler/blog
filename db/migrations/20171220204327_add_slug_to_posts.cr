af750c0488b0c8f0aeb5cbef2a41e04807ece4b5
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-12-28 14:45:44 +0100

    Temporarily move raw sql into later migration

--- db/migrations/20171220204327_add_slug_to_posts.cr
+++ db/migrations/20171220204327_add_slug_to_posts.cr
@@ -3,8 +3,6 @@ class AddSlugToPosts::V20171220204327 < LuckyMigrator::Migration::V1
     alter :posts do
       add slug : String?
     end
-
-    execute "UPDATE posts SET slug = regexp_replace(lower(title), '\\s', '-', 'g')"
   end
 
   def rollback
--- db/migrations/20171220205424_make_slug_non_nilable.cr
+++ db/migrations/20171220205424_make_slug_non_nilable.cr
@@ -1,5 +1,6 @@
 class MakeSlugNonNilable::V20171220205424 < LuckyMigrator::Migration::V1
   def migrate
+    execute "UPDATE posts SET slug = regexp_replace(lower(title), '\\s', '-', 'g')"
     execute "ALTER table posts ALTER slug SET NOT NULL"
   end
 
