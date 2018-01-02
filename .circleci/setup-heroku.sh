70bd7dd0ec3f7b04acec1d813e3516459ba3f10d
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-02 19:15:33 +0100

    Use custom docker image

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku.tar.gz
-tar -xvzf heroku.tar.gz
-mkdir -p /usr/local/lib /usr/local/bin
-mv heroku-cli-v6.14.43-73d5876-linux-x64 /usr/local/lib/heroku
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
-
 cat > ~/.netrc << EOF
 machine api.heroku.com
 login $HEROKU_LOGIN
