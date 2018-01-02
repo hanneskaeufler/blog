8e95de46ce4e1ba09eaab8b26008f342bba946dc
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-02 19:53:34 +0100

    Add credentials for heroku git as well

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,3 +4,8 @@ machine api.heroku.com
     login $HEROKU_LOGIN
     password $HEROKU_API_KEY
 EOF
+cat > ~/.netrc << EOF
+machine git.heroku.com
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
+EOF
