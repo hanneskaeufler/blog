5549418c8fd6172a82ecd082dfbd61781b70765c
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-02 20:26:37 +0100

    Add heroku to known hosts

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -4,8 +4,16 @@ machine api.heroku.com
     login $HEROKU_LOGIN
     password $HEROKU_API_KEY
 EOF
+
 cat > ~/.netrc << EOF
 machine git.heroku.com
     login $HEROKU_LOGIN
     password $HEROKU_API_KEY
+EOF
+
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+
+cat >> ~/.ssh/config << EOF
+VerifyHostKeyDNS yes
+StrictHostKeyChecking no
 EOF
