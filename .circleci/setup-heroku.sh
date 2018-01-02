8aa03c1f05bba8ea928d50cd6a766d61c54bfc04
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-02 19:22:57 +0100

    Better indentation

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cat > ~/.netrc << EOF
 machine api.heroku.com
-login $HEROKU_LOGIN
-password $HEROKU_API_KEY
+    login $HEROKU_LOGIN
+    password $HEROKU_API_KEY
 EOF
 
 cat >> ~/.ssh/config << EOF
-    VerifyHostKeyDNS yes
-    StrictHostKeyChecking no
+VerifyHostKeyDNS yes
+StrictHostKeyChecking no
 EOF
