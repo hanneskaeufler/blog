77bdfdc68edf85b2989041b520341ce95718e79f
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-03 21:42:34 +0100

    Make 404 page actually useful

--- src/pages/blog/not_found.cr
+++ src/pages/blog/not_found.cr
@@ -4,6 +4,11 @@ class Blog::NotFoundPage < MainLayout
   def inner
     section class: "posts-container" do
       h1 "Not Found"
+      para do
+        raw "You might have been looking for one of my "
+        link "blog posts", to: Blog::Posts::Index
+        raw "?"
+      end
       raw "<pre style=\"font-size: 5em;\">¯\\_(ツ)_/¯</pre>"
     end
     render_footer
