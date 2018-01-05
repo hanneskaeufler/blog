4ea773f5225e3717232c5b1015e22b4593bf0270
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-05 22:32:00 +0100

    Show posts grouped by year

--- src/pages/blog/archive.cr
+++ src/pages/blog/archive.cr
@@ -5,18 +5,25 @@ class Blog::ArchivePage < MainLayout
 
   def inner
     render_small_header
-    render_post_titles(@posts)
+    render_post_titles(posts_by_year(@posts))
     render_footer
   end
 
-  private def render_post_titles(posts)
+  private def posts_by_year(posts)
+    posts.group_by { |post| post.published_at.year  }
+  end
+
+  private def render_post_titles(posts_by_year)
     section class: "posts-archive" do
       h2 "Archive"
       ul do
-        @posts.each do |post|
-          li do
-            span post.published_at.to_s("%Y-%m-%d")
-            link post.title, to: Blog::Posts::Show.with(post.slug)
+        posts_by_year.each do |year, posts|
+          h3 year
+          posts.each do |post|
+            li do
+              span post.published_at.to_s("%b %-d")
+              link post.title, to: Blog::Posts::Show.with(post.slug)
+            end
           end
         end
       end
