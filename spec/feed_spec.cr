9fcee73d7c8259138cfdd68ed75acd921f9e0e1d
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-05-21 15:28:51 +0200

    Host is now localhost for test/dev

--- spec/feed_spec.cr
+++ spec/feed_spec.cr
@@ -2,6 +2,7 @@ require "./spec_helper"
 
 describe Blog::Feed do
   visitor = AppVisitor.new
+  host = "http://0.0.0.0:5000"
 
   describe "/feed.json" do
     it "renders a valid json feed" do
@@ -14,17 +15,17 @@ describe Blog::Feed do
 
       response.body.should eq({
         "version":       "https://jsonfeed.org/version/1",
-        "home_page_url": "http://hannesdotkaeuflerdotnet.herokuapp.com/",
-        "feed_url":      "http://hannesdotkaeuflerdotnet.herokuapp.com/feed.json",
+        "home_page_url": "#{host}/",
+        "feed_url":      "#{host}/feed.json",
         "description":   Blog::Components::TAGLINE,
         "author":        {"name": "Hannes Käufler"},
-        "favicon":       "http://hannesdotkaeuflerdotnet.herokuapp.com/favicon.ico",
+        "favicon":       "#{host}/favicon.ico",
         "items":         [
           {
             "id":             "#{post_id}",
             "title":          "post title",
             "content_html":   "<h2>post</h2>\n\n<p>content</p>",
-            "url":            "http://hannesdotkaeuflerdotnet.herokuapp.com/posts/post-title",
+            "url":            "http://0.0.0.0:5000/posts/post-title",
             "date_published": "1970-01-03T00:01:00+0000",
             "date_updated":   "1970-01-04T00:00:50+0000",
           },
@@ -38,7 +39,7 @@ describe Blog::Feed do
 
         response = visitor.visit("/feed.json", headers)
 
-        JSON.parse(response.body)["next_url"].as_s.should eq "http://hannesdotkaeuflerdotnet.herokuapp.com/feed.json?page=2"
+        JSON.parse(response.body)["next_url"].as_s.should eq "#{host}/feed.json?page=2"
       end
 
       it "does not show a next_url when on the last page" do
