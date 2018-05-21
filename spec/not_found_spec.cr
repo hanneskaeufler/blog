531a0dabb21ac2cb23a4ffddefd5964f6643bac8
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-05-21 17:15:45 +0200

    Verify 404 not found page for missing posts

--- spec/not_found_spec.cr
+++ spec/not_found_spec.cr
@@ -9,4 +9,14 @@ describe Blog do
     visitor.should contain "<h1>Not Found</h1>"
     visitor.response.status_code.should eq 404
   end
+
+  it "renders a not found page for invalid posts" do
+    Lucky::ErrorHandler.configure { settings.show_debug_output = false }
+    visitor = AppVisitor.new
+
+    visitor.visit("/posts/not-there")
+
+    visitor.should contain "<h1>Not Found</h1>"
+    visitor.response.status_code.should eq 404
+  end
 end
