35c0ed84f9d15a62b2e70ee038fe7b38ec8e455a
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-05-23 22:33:22 +0200

    Add comment about APP_DOMAIN env var

--- config/route_helper.cr
+++ config/route_helper.cr
@@ -1,5 +1,6 @@
 Lucky::RouteHelper.configure do
   if Lucky::Env.production?
+    # APP_DOMAIN must be set in heroku
     settings.base_uri = ENV.fetch("APP_DOMAIN")
   else
     settings.base_uri = "http://#{Lucky::ServerSettings.host}:#{Lucky::ServerSettings.port}"
