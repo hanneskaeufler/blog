63fc278927056afc3465bcf386b2368330e35ec4
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-07-02 19:55:47 +0200

    Format fixes

--- db/migrations/20180627211022_move_posts_to_uuid.cr
+++ db/migrations/20180627211022_move_posts_to_uuid.cr
@@ -2,7 +2,7 @@ class MovePostsToUUID::V20180627211022 < LuckyMigrator::Migration::V1
   def migrate
     execute "ALTER TABLE posts ADD COLUMN new_id uuid"
     PostQuery.new.each do |post|
-      new_id = UUID.random()
+      new_id = UUID.random
       execute "UPDATE posts set new_id = '#{new_id}' where id = #{post.id}"
     end
     execute "ALTER TABLE posts DROP COLUMN id"
