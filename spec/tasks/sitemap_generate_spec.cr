ba1e66bb7250ca71c316057a5825617fb1d13050
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-11 21:37:01 +0200

    Assert basics on sitemap content

--- spec/tasks/sitemap_generate_spec.cr
+++ spec/tasks/sitemap_generate_spec.cr
@@ -1,13 +1,27 @@
+require "gzip"
+require "xml"
 require "../spec_helper"
 require "../../tasks/sitemap_generate"
 
 SITEMAP = "./sitemap.xml.gz"
 
 describe Sitemap::Generate do
-  it "generates a sitemap" do
+  it "generates a gzipped sitemap" do
     File.delete(SITEMAP) if File.exists?(SITEMAP)
     Sitemap::Generate.new.call
 
     File.exists?(SITEMAP).should eq true
+  end
+
+  it "contains an entry for root, archive and each post" do
+    insert_post title: "earlier_post", published_at: Time.now - 4.days
+
+    Sitemap::Generate.new.call
+
+    Gzip::Reader.open(SITEMAP) do |gzip|
+      document = XML.parse(gzip.gets_to_end)
+      urls = document.first_element_child.not_nil!.children.select(&.element?)
+      urls.size.should eq 3
+    end
   end
 end
