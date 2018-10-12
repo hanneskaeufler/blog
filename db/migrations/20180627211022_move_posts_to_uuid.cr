a0f06f56fc12da43d00573c0ff23d900599d2656
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-10-12 20:48:03 +0200

    Wip work for cleaner migration

--- db/migrations/20180627211022_move_posts_to_uuid.cr
+++ db/migrations/20180627211022_move_posts_to_uuid.cr
@@ -1,13 +1,15 @@
-class MovePostsToUUID::V20180627211022 < LuckyMigrator::Migration::V1
+class MovePostsToUUID::V20180627211022 < LuckyRecord::Migrator::Migration::V1
   def migrate
-    execute "ALTER TABLE posts ADD COLUMN new_id uuid"
-    PostQuery.new.each do |post|
-      new_id = UUID.random
-      execute "UPDATE posts set new_id = '#{new_id}' where id = #{post.id}"
+    alter :posts do
+      add new_id : UUID
     end
-    execute "ALTER TABLE posts DROP COLUMN id"
-    execute "ALTER TABLE posts RENAME COLUMN new_id TO id"
-    execute "ALTER TABLE posts add PRIMARY KEY (id)"
+    # PostQuery.new.each do |post|
+    #   new_id = UUID.random
+    #   execute "UPDATE posts set new_id = '#{new_id}' where id = #{post.id}"
+    # end
+    # execute "ALTER TABLE posts DROP COLUMN id"
+    # execute "ALTER TABLE posts RENAME COLUMN new_id TO id"
+    # execute "ALTER TABLE posts add PRIMARY KEY (id)"
   end
 
   def rollback
