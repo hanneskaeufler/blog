5c39f2c8f0b8c5a33cc6a0ad13f8b8b354ee2411
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-12-25 22:41:13 +0100

    Remove unused code, we never deal with json

--- src/actions/errors/show.cr
+++ src/actions/errors/show.cr
@@ -1,12 +1,4 @@
 class Errors::Show < Lucky::ErrorAction
-  def handle_error(error : JSON::ParseException)
-    if json?
-      json({error: "There was a problem parsing the JSON. Please check that it is formed correctly"}, status: 400)
-    else
-      head status: 400
-    end
-  end
-
   def handle_error(error : LuckyRecord::RecordNotFoundError)
     context.response.status_code = 404
     render Blog::NotFoundPage
