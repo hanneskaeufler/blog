5c22a0ad3b2d04ec55d5716c6dc1cc38e55ab93e
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-07-07 13:41:07 +0200

    Extract google analytics component

--- src/pages/main_layout.cr
+++ src/pages/main_layout.cr
@@ -14,13 +14,13 @@ abstract class MainLayout
         utf8_charset
         csrf_meta_tags
         responsive_meta_tag
-        meta rel: "alternate", title: "Racing, Software, Open-Source", href: "/feed.json"
+        meta rel: "alternate", title: Blog::Components::Tagline::TAGLINE, href: "/feed.json"
         css_link(asset("images/favicon.png"), rel: "icon")
         title page_title
         css_link asset("css/app.css")
         js_link asset("js/app.js")
         inner_head
-        google_analytics if Lucky::Env.production?
+        mount Blog::Components::GoogleAnalytics.new(Lucky::Env.production?)
       end
 
       body do
@@ -36,18 +36,5 @@ abstract class MainLayout
 
   def page_title_suffix
     ""
-  end
-
-  def google_analytics
-    raw <<-RAW
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-10194989-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-10194989-1');
-      </script>
-    RAW
   end
 end
