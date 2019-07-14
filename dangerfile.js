0bdbb46f604ac4e7c2e816382f6ca80e2e04ad65
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-07-14 20:25:36 +0200

    Add a bunch of custom crystal tips for funsies

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,6 @@
-import mentor from "danger-plugin-mentor"
+import { fromJson, default as mentor } from "danger-plugin-mentor"
 import { message, warn, fail, markdown } from "danger"
+import fs from "fs"
 
 if (danger.github.pr.title.indexOf("[WIP]") !== -1) {
   warn("PR is classified as Work in Progress");
@@ -10,4 +11,4 @@ if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
   warn(":exclamation: Big PR");
 }
 
-mentor()
+mentor(undefined, fromJson(fs.readFileSync("./crystal-lang-tips.json")))
