acb36c911126d7674599f9db9b917a2d427c3b4f
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2019-09-08 17:59:26 +0200

    wip

--- src/components/blog/intro.cr
+++ src/components/blog/intro.cr
@@ -1,12 +1,20 @@
 class Blog::Components::Intro < BaseComponent
   def render
     section class: "intro" do
-      ul do
-        SOCIALS.each do |name, href|
-          li do
-            link to: href do
-              img src: dynamic_asset("logos/#{name}.svg"), alt: "social logo #{name}"
-              text name.to_s
+      nav do
+        ul class: "navigation" do
+          li { link to: "#" { text "Blog Archive "; raw "&raquo;" } }
+        end
+
+        div do
+          ul class: "social" do
+            SOCIALS.each do |name, href|
+              li do
+                link to: href do
+                  img src: dynamic_asset("logos/#{name}.svg"), alt: "social logo #{name}"
+                  text name.to_s
+                end
+              end
             end
           end
         end
