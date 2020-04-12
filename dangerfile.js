7f64914d4091b4332b81bdf143efa1f647c92e71
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-04-12 16:59:56 +0200

    Move danger-plugin-mentor from local to actions

--- dangerfile.js
+++ dangerfile.js
@@ -1,6 +1,5 @@
-import { fromJson, default as mentor, defaultTips } from "danger-plugin-mentor"
-import { message, warn, fail, markdown } from "danger"
-import fs from "fs"
+import { message, warn, fail, markdown } from "danger";
+import fs from "fs";
 
 if (danger.github.pr.title.indexOf("[WIP]") !== -1) {
   warn("PR is classified as Work in Progress");
@@ -10,5 +9,3 @@ var bigPRThreshold = 200;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
   warn(":exclamation: Big PR");
 }
-
-mentor(undefined, [...defaultTips(), ...fromJson(fs.readFileSync("./crystal-lang-tips.json"))])
