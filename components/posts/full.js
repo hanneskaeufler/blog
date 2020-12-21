e9560a6cf2466222767985f9861757766908b175
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-12-21 23:35:19 +0100

    Set page title for posts

--- components/posts/full.js
+++ components/posts/full.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import PostHead from "@/components/posts/head";
 import PostBody from "@/components/posts/body";
 
 export default function Full({ post }) {
   return (
     <div className="container mx-auto flex-grow">
+      <Head>
+        <title>Hannes Käufler: Blog - {post.title}</title>
+      </Head>
       <PostHead post={post} />
       <PostBody post={post} />
     </div>
