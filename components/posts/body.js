aa2ebf5c7a6360367cfe4a3831d115d83d3e0dcf
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-12-21 23:27:40 +0100

    Improve post rendering

--- components/posts/body.js
+++ components/posts/body.js
@@ -4,7 +4,11 @@ import gfm from "remark-gfm";
 export default function Body({ post }) {
   return (
     <div className="mb-4 prose">
-      <ReactMarkdown plugins={[gfm]} children={post.content} />
+      <ReactMarkdown
+        plugins={[gfm]}
+        children={post.content}
+        allowDangerousHtml={true} // I embed some raw html, script tags e.g. for github gists
+      />
     </div>
   );
 }
