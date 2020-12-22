2f147b05fde0fdaef4e8985c2ddcea1167dd333b
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-12-22 13:38:53 +0100

    Export a bunch more posts

---
+++ pages/posts/lucky-crystal.js
@@ -0,0 +1,19 @@
+export const post = {
+  slug: "lucky-crystal",
+  title: "Lucky Crystal",
+  publishedAt: "2017-12-03 11:50:31.000629+00",
+  content: `In the ever changing landscape of web frameworks, there is a new kid on the block: [Lucky](http://luckyframework.org). Lucky is written in [Crystal](http://crystal-lang.org), a statically typed programming language heavily inspired by [Ruby's](http://ruby-lang.org) syntax, which claims to be
+> Fast as C, slick as Ruby
+
+For this blog however, being as fast as C is mostly irrelevant, I'm sure it'd be plenty fine running in Ruby as well. What I rather value is developer productivity, or "getting something done in half an hour before going to bed". On that end, the Lucky + Crystal combination performed pretty well and I got a rewrite of this website shipped in a few hours of work.
+
+## Type safe html
+
+What I find super interesting about Lucky is the way it deals with the task of rendering html. While the plethora of templating engines like [ERB](http://ruby-doc.org/stdlib-2.4.2/libdoc/erb/rdoc/ERB.html), [Twig](https://twig.symfony.com) or Crystal's [ECR](https://crystal-lang.org/api/0.23.1/ECR.html) are easy to work with and simple enough to understand, there's something about using plain Crystal code to markup html. The following gist is an example method from this blogs source code:
+
+<!-- RAW_HTML_START --><script src="https://gist.github.com/hanneskaeufler/df48f4ee11c332c92efbc157c85174e9.js"></script><!-- RAW_HTML_END -->
+
+As you can see it was trivial to extract methods to render reoccuring or more complex parts of the layout. It's just code. Also the compiler will complain if we produce invalid code, so we cannot forget to close tags. That's kind of a big deal to me.
+
+I look forward to spending more time in Lucky and Crystal, they both get a thumbs up for now!`,
+};
---
+++ pages/posts/my-2017-podcast-winners.js
@@ -0,0 +1,12 @@
+export const post = {
+  slug: "my-2017-podcast-winners",
+  title: "My 2017 podcast winners",
+  publishedAt: "2017-12-22 22:38:49+00",
+  content: `In 2017 I have likely listened to hundreds of hours of podcasts. Out of interest, lets do the math real quick: \`50 weeks until now * 5 podcasts * 1h average length = 250h\`. So yeah, hundreds of hours it is. But I definitely don't consider that to be wasted time, but sometimes great entertainment, time spent learning, or a soothing tone to fall asleep to. Without much further ado, here's what I have been listening to in 2017, in no particular order:
+
+* [2 Dope Queens](https://overcast.fm/itunes1097193327/2-dope-queens): Phoebe Robinson and Jessica Williams are two friends hosting a hilarious live comedy show. They literally crack me up each episode and easily brighten my mood for quite a while. Funniest podcast I know.
+* [Scrum Master Toolbox Podcast](https://overcast.fm/itunes963592988/scrum-master-toolbox-podcast): Each episode is around eleven minutes long and is a concise discussion about the daily work and struggle of the scrum master trade. Vasco Duarte does a great job in keeping those episodes short and to the point. Great way to learn some useful tips from more experienced agile coaches/scrum masters.
+* [Missed Apex Podcast](https://overcast.fm/itunes1097310026/missed-apex-podcast): This was a new discovery for me in 2017 and made the formula 1 season so much more enjoyable. The f1 race reviews are both funny and informative. The host Spanners Ready leads a crew of journalists and f1 fans to produce these reviews but also more technical shows and interviews with people of f1 fame. A must listen!
+* [The Bike Shed](https://overcast.fm/itunes935763119/the-bike-shed): This has nothing to do with bikes, but is a semi-random conversation between Derek Prior, Sean Griffin, Amanda Hill and various guests on IT topics such as Ruby on Rails, Active Record, Diesel, mixed with stories on consulting work, rockets and anything else that might come up. I find it provides a nice mix of interesting technical discussions and light-hearted banter.
+* [Accidental Tech Podcast](https://overcast.fm/itunes617416468/accidental-tech-podcast): The trio of Casey Liss, Marco Arment and John Siracusa do a great job of endlessly discussing the world of Apple and related surrounding topics. This is definitely the podcast I have been following for the longest time.`,
+};
