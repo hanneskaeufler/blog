5a10952687d5f10f55e50cc5bbbfd283f50d4b96
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-12-27 22:41:28 +0100

    Slight copy improvements

--- pages/posts/chrome-vs-safari.js
+++ pages/posts/chrome-vs-safari.js
@@ -6,7 +6,7 @@ export const post = {
   title: "Chrome vs Safari",
   publishedAt: "2016-06-01 14:23:28.167643+00",
   content: `Chrome and Safari are both great browsers. They each have their pros and cons. I tend to be undecided on which one I like better.
-Chrome has great [developer tools](https://developer.chrome.com/devtools). I never really got warm with Safari's. As usual, Safari is tightly integrated
+Chrome has great [developer tools](https://developer.chrome.com/devtools). I never really warmed up to Safari's. As usual, Safari is tightly integrated
 with OS X. Tab sync to iOS's Safari being one of them. Generally using less memory. Lately I tend to use Safari more then Chrome. But out of old habits
 I often start a new window in Chrome and browse for a few minutes before remembering that I wanted to use Safari. So here it is, the solution ~~to all of my problems~~
 to a tiny annoyance of my incredibly blessed life:
