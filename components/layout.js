e9aeed4f5dc8347c6f6bd6586f074db7f4827b23
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-12-27 22:37:00 +0100

    Tweak layout to only show tagline

--- components/layout.js
+++ components/layout.js
@@ -5,11 +5,7 @@ export default function Layout({ children }) {
     <div className="flex-grow">
       <div className="py-4 bg-green-300 mb-14 text-sm font-bold relative">
         <div className="container mx-auto">
-          <Link href="/">
-            <a className="inline-block border-b border-green-400 text-green-800 hover:border-green-800 transition">
-              Home
-            </a>
-          </Link>
+          <div className="text-green-600">racing · software · open-source</div>
         </div>
       </div>
       {children}
