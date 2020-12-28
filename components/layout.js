16335c80385c2fce0f86497f000ef343fca79e3a
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-12-28 16:21:28 +0100

    Make tagline a home link

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,13 @@ export default function Layout({ children }) {
     <div className="flex-grow">
       <div className="py-4 bg-green-300 mb-14 text-sm font-bold relative">
         <div className="container mx-auto">
-          <div className="text-green-600">racing · software · open-source</div>
+          <div className="text-green-600">
+            <Link href="/">
+              <a className="border-b border-transparent hover:border-green-500 transition">
+                racing · software · open-source
+              </a>
+            </Link>
+          </div>
         </div>
       </div>
       {children}
