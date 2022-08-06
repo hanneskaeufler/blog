c966ed6d10d8915ab7f5e7ffb17bb2df768e6557
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2022-08-06 20:49:00 +0200

    Fix colors. green is now emerald
    See tailwind upgrade guide

--- components/layout.js
+++ components/layout.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <div className="flex-grow">
-      <div className="py-4 bg-green-300 mb-14 text-sm font-bold relative">
+      <div className="py-4 bg-emerald-300 mb-14 text-sm font-bold relative">
         <div className="container mx-auto px-2">
-          <div className="text-green-600">
+          <div className="text-emerald-600">
             <Link href="/">
-              <a className="border-b border-transparent hover:border-green-500 transition">
+              <a className="border-b border-transparent hover:border-emerald-500 transition">
                 racing · software · open-source
               </a>
             </Link>
