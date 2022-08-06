823cf7c281da30d59baf93dafa9aa289446c3d73
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2022-08-06 20:36:46 +0200

    Prevent warning about multiple title tags being rendered
    https://github.com/vercel/next.js/discussions/38256

--- components/posts/full.js
+++ components/posts/full.js
@@ -3,10 +3,11 @@ import PostHead from "@/components/posts/head";
 import PostBody from "@/components/posts/body";
 
 export default function Full({ post }) {
+  const title = `Hannes Käufler: Blog - {post.title}`;
   return (
     <div className="container mx-auto px-2">
       <Head>
-        <title key="title">Hannes Käufler: Blog - {post.title}</title>
+        <title key="title">{title}</title>
       </Head>
       <PostHead post={post} />
       <PostBody content={post.content} />
