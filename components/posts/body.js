eddcea026d740c5050b56a64c55b3582152c46ad
anupamme <mediratta@gmail.com>
2026-09-12 08:20:00 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- components/posts/body.js
+++ components/posts/body.js
@@ -2,12 +2,13 @@
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
 
 export default function Body({ content }) {
   return (
     <div className="mb-4 prose prose-indigo prose-lg">
       <ReactMarkdown
-        rehypePlugins={[rehypeRaw]}
+        rehypePlugins={[rehypeRaw, [rehypeSanitize, defaultSchema]]}
         remarkPlugins={[gfm]}
         children={content}
       />
